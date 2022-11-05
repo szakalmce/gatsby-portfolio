997036eb1cd5369cd10657aa9df3c1bcd494ec43
unknown <tomasz.garbarz9@gmail.com>
2022-11-05 16:44:04 +0100

    next commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,16 @@ const contact = () => {
       <div className="container h-100">
         <div className="contact-inner">
           <div className="form-wrapper">
-            <form className="form">
+            <form
+              className="form"
+              name="portfolio-form"
+              method="post"
+              netlify-honeypot="bot-field"
+              data-netlify="true"
+              action="/"
+            >
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="porfolio-name" />
               <div className="form-inner">
                 <p className="form-title">Get In Touch</p>
                 <input type="text" placeholder="NAME" />
